e539543f852be9cb650082ab31ac107dd1e91042
asimoq <112816411+asimoq@users.noreply.github.com>
2023-10-09 18:22:35 +0200

    Add project files.

---
+++ ABC123_HFT_2023241.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ABC123_HFT_2023241.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ TAG8GJ_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAG8GJ_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
