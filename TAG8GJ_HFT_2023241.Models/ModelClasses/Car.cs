be82fe178bdb89feca3bea3861c46e0e97c93e0f
asimoq <112816411+asimoq@users.noreply.github.com>
2023-11-24 18:37:45 +0100

    JsonIgnore atribútumok beállítása

--- TAG8GJ_HFT_2023241.Models/ModelClasses/Car.cs
+++ TAG8GJ_HFT_2023241.Models/ModelClasses/Car.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 
@@ -29,7 +30,7 @@ namespace TAG8GJ_HFT_2023241.Models
 
         [Range(0, 300000)]
         public int DailyRentalCost { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<Rental> Rentals { get; set; }
 
 
--- TAG8GJ_HFT_2023241.Models/ModelClasses/Customer.cs
+++ TAG8GJ_HFT_2023241.Models/ModelClasses/Customer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TAG8GJ_HFT_2023241.Models
@@ -22,7 +23,7 @@ namespace TAG8GJ_HFT_2023241.Models
 
         [StringLength(30)]
         public string CustomerPhone { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<Rental> Rentals { get; set; }
 
 
